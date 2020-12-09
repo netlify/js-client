3d852630d24aa86375e4a4bcf69334bbdd4e8878
ehmicky <ehmicky@gmail.com>
2020-12-09 20:54:11 +0100

    Add `array-callback-return` ESLint rule

--- src/deploy/hash-files.test.js
+++ src/deploy/hash-files.test.js
@@ -10,9 +10,13 @@ test('hashes files in a folder', async (t) => {
     filter: defaultFilter,
   })
   t.truthy(files['netlify.toml'], 'includes the netlify.toml file')
-  Object.keys(files).forEach((path) => t.truthy(path, 'each file has a path'))
+  Object.keys(files).forEach((path) => {
+    t.truthy(path, 'each file has a path')
+  })
   t.truthy(filesShaMap, 'filesShaMap is returned')
-  Object.values(filesShaMap).forEach((fileObjArray) =>
-    fileObjArray.forEach((fileObj) => t.truthy(fileObj.normalizedPath, 'fileObj have a normalizedPath field')),
-  )
+  Object.values(filesShaMap).forEach((fileObjArray) => {
+    fileObjArray.forEach((fileObj) => {
+      t.truthy(fileObj.normalizedPath, 'fileObj have a normalizedPath field')
+    })
+  })
 })
