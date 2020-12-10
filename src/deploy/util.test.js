bc573ce7e602b7104ce4a01e6d8ac11ba4b91bf3
ehmicky <ehmicky@gmail.com>
2020-12-10 18:15:11 +0100

    Simplify a unit test

--- src/deploy/util.test.js
+++ src/deploy/util.test.js
@@ -1,27 +1,12 @@
+const { join } = require('path')
+
 const test = require('ava')
 
 const { normalizePath, defaultFilter } = require('./util')
 
 test('normalizes relative file paths', (t) => {
-  const cases = [
-    {
-      input: 'foo/bar/baz.js',
-      expect: 'foo/bar/baz.js',
-      msg: 'relative paths are normalized',
-      skip: process.platform === 'win32',
-    },
-    {
-      input: 'beep\\impl\\bbb',
-      expect: 'beep/impl/bbb',
-      msg: 'relative windows paths are normalized',
-      skip: process.platform !== 'win32',
-    },
-  ]
-
-  cases.forEach((c) => {
-    if (c.skip) return
-    t.is(normalizePath(c.input), c.expect, c.msg)
-  })
+  const input = join('foo', 'bar', 'baz.js')
+  t.is(normalizePath(input), 'foo/bar/baz.js')
 })
 
 test('normalizePath should throw the error if name is invalid', (t) => {
