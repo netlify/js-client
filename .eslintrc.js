e332ff9226e316679bb9d6c2c4e4d24513bd6cd3
ehmicky <ehmicky@gmail.com>
2020-12-18 17:33:21 +0100

    chore: enable `fp/no-class` ESLint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,9 @@ module.exports = {
     // TODO: enable those rules
     complexity: 0,
     'max-statements': 0,
-    'fp/no-class': 0,
     'fp/no-let': 0,
     'fp/no-loops': 0,
     'fp/no-mutation': 0,
-    'fp/no-this': 0,
     'promise/no-callback-in-promise': 0,
     'promise/prefer-await-to-callbacks': 0,
   },
